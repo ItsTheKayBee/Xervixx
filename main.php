97ef395de1f0caf74e1d0b5da1cb38dd82a49b85
Kunal <kunal.bohra@somaiya.edu>
2019-07-12 13:09:40 +0530

    minor tweaks

--- main.php
+++ main.php
@@ -8,7 +8,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
     body {font-family: "Lato", sans-serif}
-    .mySlides {display: none}
     p{cursor: default}
 </style>
 <body>
@@ -34,12 +33,13 @@
     <a href="login.php" class="w3-bar-item w3-button w3-padding-large" >LOG IN</a>
     <a href="register.php" class="w3-bar-item w3-button w3-padding-large" >REGISTER</a>
 </div>
-<div class="w3-content w3-black" style="max-width:2000px;margin-top:46px">
-    <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+    <div class="w3-padding-32"></div>
+    <div class="w3-container w3-black w3-content w3-center" style="max-width:800px;padding-top: 30px" id="band">
         <img class="w3-center" alt="xervixx-logo" src='xervixx-logo.png' style="z-index:30;width: 400px;height: 120px">
         <p class="w3-opacity"><i>WE LOVE TO SEE YOUR DREAMS COME TRUE</i></p>
         <p class="w3-justify"></p>
-        <div class="w3-row-padding w3-padding-32">
+        <div class="w3-row-padding" style="padding-top: 30px;font-weight: bolder">
             <div class="w3-quarter w3-col w3-card-4 w3-black w3-hover">
                 <p CLASS="w3-wide">PROTECTING</p>
                 <img src="" class="w3-round w3-margin-bottom" alt="" style="width:60%">
@@ -58,6 +58,7 @@
             </div>
         </div>
     </div>
+    <div class="w3-padding-32"></div>
     <div class="w3-black" id="loan">
         <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
             <h1 class="w3-center w3-wide">LOAN</h1>
@@ -107,24 +108,6 @@
             </div>
         </div>
     </div>
-    <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="emi">
-        <h2 class="w3-wide w3-center">EASY MONTHLY INSTALLMENTS- THE GAME</h2>
-        <p class="w3-opacity w3-center"><i>GOTTA WIN EVERY LEVEL!</i></p>
-        <div class="w3-row w3-center w3-padding-32">
-            <div class="w3-col w3-third">
-                <img src="rate.png"  width="120px">
-                <p> PAY ALL EMIs ON TIME TO GET STARS</p>
-            </div>
-            <div class="w3-col w3-third">
-                <img src="success.png" width="120px">
-                <p>REACH AT THE TOP OF THE LEADERBOARD</p>
-            </div>
-            <div class="w3-col w3-third">
-                <img src="money.png" width="120px">
-                <p>GET CASHBACK ON YOUR LOAN</p>
-            </div>
-        </div>
-    </div>
     <div class="w3-black" id="feed">
         <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
             <h2 class="w3-wide w3-center">THE FEED</h2>
@@ -145,28 +128,48 @@
             </div>
         </div>
     </div>
-    <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="emi">
+        <h2 class="w3-wide w3-center">EASY MONTHLY INSTALLMENTS- THE GAME</h2>
+        <p class="w3-opacity w3-center"><i>GOTTA WIN EVERY LEVEL!</i></p>
+        <div class="w3-row w3-center w3-padding-32">
+            <div class="w3-col w3-third">
+                <img src="rate.png"  width="120px">
+                <p> PAY ALL EMIs ON TIME TO GET STARS</p>
+            </div>
+            <div class="w3-col w3-third">
+                <img src="success.png" width="120px">
+                <p>REACH AT THE TOP OF THE LEADERBOARD</p>
+            </div>
+            <div class="w3-col w3-third">
+                <img src="money.png" width="120px">
+                <p>GET CASHBACK ON YOUR LOAN</p>
+            </div>
+        </div>
+    </div>
+    <div class="w3-black" id="feed">
         <h2 class="w3-wide w3-center">CONTACT</h2>
         <p class="w3-opacity w3-center"><i>WE WOULD LOVE TO HEAR FROM YOU!</i></p>
-        <div class="w3-row w3-padding-32">
-            <div class="w3-col m6 w3-large w3-margin-bottom">
-                <i class="fa fa-map-marker" style="width:30px"></i> Mumbai, India<br>
-                <i class="fa fa-phone" style="width:30px"></i> Phone: +91 1234567890<br>
-                <i class="fa fa-envelope" style="width:30px"> </i> Email: support@xervixx.com<br>
-            </div>
-            <div class="w3-col m6">
-                <form target="_blank">
-                    <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
-                        <div class="w3-half">
-                            <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+        <div class="w3-container w3-content w3-padding-64" style="width: 120%" id="contact">
+            <div class="w3-row w3-padding-32">
+                <div class="w3-col m6 w3-large w3-margin-bottom">
+                    <i class="fa fa-map-marker" style="width:30px"></i> Mumbai, India<br>
+                    <i class="fa fa-phone" style="width:30px"></i> Phone: +91 1234567890<br>
+                    <i class="fa fa-envelope" style="width:30px"> </i> Email: support@xervixx.com<br>
+                </div>
+                <div class="w3-col m6">
+                    <form target="_blank">
+                        <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+                            <div class="w3-half">
+                                <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+                            </div>
+                            <div class="w3-half">
+                                <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+                            </div>
                         </div>
-                        <div class="w3-half">
-                            <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
-                        </div>
-                    </div>
-                    <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
-                    <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
-                </form>
+                        <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+                        <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
